5ee997868d43b29141598c45aaa458d37f61f226
elien <elien9omar@yaoo.com>
2021-07-03 02:40:27 +0300

    fourth

--- public/index.php
+++ public/index.php
@@ -43,7 +43,17 @@ require __DIR__.'/../vendor/autoload.php';
 | to this client's browser, allowing them to enjoy our application.
 |
 */
+/*
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
+*/
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
